0dc4120279cb245359b28ad0ca46b5b6a01084d8
Kaushasomaiya <110300468+Kaushasomaiya@users.noreply.github.com>
2022-08-01 18:17:55 +0530

    Committing project.

---
+++ test-and-taste-developer-exercise-main/Test-Taste-Console-Application/Constants/LoggerMessage.cs
@@ -0,0 +1,16 @@
+﻿namespace Test_Taste_Console_Application.Constants
+{
+    public static class LoggerMessage
+    {
+        public const string GetRequestFailed = "Get request failed with the code: ";
+
+        public const string ScreenOutputOperationFailed =
+            "The screen output operation failed with the following error: ";
+
+        public const string FileOutputOperationFailed = "The file output operation failed with the following error: ";
+        public const string APICallStart = "Loading Data : fetch data from API.";
+        public const string APICallEnd = "Loading Data END : Data is successfully fetch from API and also performed additional logic.";
+        public const string WriteData = "Writing Data to console.";
+        public const string EndWriteData = "Data is successfully written.";
+    }
+}
---
+++ test-and-taste-developer-exercise-main/Test-Taste-Console-Application/Domain/Objects/Planet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Test_Taste_Console_Application.Domain.DataTransferObjects;
+
+namespace Test_Taste_Console_Application.Domain.Objects
+{
+    public class Planet
+    {
+        public string Id { get; set; }
+        public float SemiMajorAxis { get; set; }
+        public ICollection<Moon> Moons { get; set; }
+        //public float AverageMoonGravity
+        //{
+        //    get => 0.0f;
+        //}
+
+        // Created property with get;set;
+        public float AverageMoonGravity { get; set; }
+        public float Grevity { get; set; }
+        public Planet(PlanetDto planetDto)
+        {
+            Id = planetDto.Id;
+            SemiMajorAxis = planetDto.SemiMajorAxis;
+            Grevity = planetDto.Gravity;
+            Moons = new Collection<Moon>();
+            if(planetDto.Moons != null)
+            {
+                foreach (MoonDto moonDto in planetDto.Moons)
+                {
+                    Moons.Add(new Moon(moonDto));
+                }
+            }
+        }
+
+        public Boolean HasMoons()
+        {
+            return (Moons != null && Moons.Count > 0);
+        }
+    }
+}
